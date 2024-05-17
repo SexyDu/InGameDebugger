e329f2445f5b46e2bd7e380e71a69d060237a8e4
SongDuHyeon <128912129+SexyDu@users.noreply.github.com>
2024-05-18 00:27:50 +0900

    Cmd 기능 진행 중

---
+++ Assets/SexyDu/InGameConsole/Scripts/Command/Test.Command.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SexyDu.InGameConsole
+{
+    public class TestCommand : MonoBehaviour
+    {
+        private Dictionary<string, Action<string>> cmdDictionary = new Dictionary<string, Action<string>>();
+
+        public void Add(string cmd, Action<string> act)
+        {
+            if (cmdDictionary.ContainsKey(cmd))
+                Debug.LogWarningFormat($"Command '{cmd}'가 이미 정의되어 있습니다.");
+            else
+                cmdDictionary.Add(cmd, act);
+        }
+
+        public void Remove(string cmd)
+        {
+            cmdDictionary.Remove(cmd);
+        }
+
+        public string[] GetMatchedCommands(string keyword)
+        {
+            string patternCore = $"{keyword.ToLower()}.*";
+            string patternContain = $".*{keyword.ToLower()}.*";
+
+            int coreIndex = 0;
+            List<string> list = new List<string>();
+
+            foreach (string cmd in cmdDictionary.Keys)
+            {
+                if (Regex.IsMatch(cmd, patternCore))
+                {
+                    list.Insert(coreIndex++, cmd);
+                }
+                else if (Regex.IsMatch(cmd, patternContain))
+                {
+                    list.Add(cmd);
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
--- Assets/SexyDu/InGameConsole/Scripts/Settings/InGameConsoleSettings.cs
+++ Assets/SexyDu/InGameConsole/Scripts/Settings/InGameConsoleSettings.cs
@@ -9,8 +9,22 @@ namespace SexyDu.InGameConsole
     [CreateAssetMenu(fileName = "InGameConsoleSettings", menuName = "SexyDu/Settings/InGameConsole")]
     public class InGameConsoleSettings : ScriptableObject
     {
+        [Tooltip("InGameConsole에 사용될 폰트에셋")]
         [SerializeField] private TMP_FontAsset fontAsset;
         public TMP_FontAsset FontAsset { get { return fontAsset; } }
+
+        [Tooltip("로그 Scroll 시 로그 출력 Pause 여부\n* 체크되어 있는 경우 스크롤 시 로그 출력이 멈춥니다.")]
+        [SerializeField] private bool useScrollPause = true;
+        public bool UseScrollPause { get { return useScrollPause; } }
+
+        [Header("Canvas")]
+        [Tooltip("InGameConsole의 Sorting layer")]
+        [SerializeField] private string sortingLayer;
+        public string SortingLayer { get { return sortingLayer; } }
+        
+        [Tooltip("InGameConsole의 Order in layer")]
+        [SerializeField] private int orderInLayer;
+        public int OrderInLayer { get { return orderInLayer; } }
     }
 
 #if UNITY_EDITOR
